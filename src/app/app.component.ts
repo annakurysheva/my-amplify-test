82190e484a0111ad32e56e33a15198ef61d0007f
Anna Kurysheva <anna.kurysheva@adp.com>
2021-03-23 19:48:19 +0100

    test PR preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Hello Amplify Test';
+  title = 'Test PR Preview - Hello Amplify Test';
 }
